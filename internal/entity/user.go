115070475976eadb63c4ddcf35653f71ad53e4e3
Boris <b.mirzakhanyan@andersenlab.com>
2022-07-21 22:14:47 +0300

    marshal user

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,9 +1,21 @@
 package entity
 
+import tele "gopkg.in/telebot.v3"
+
 type User struct {
 	ID           int64  `json:"id"`
 	FirstName    string `json:"first_name"`
 	LastName     string `json:"last_name"`
 	Username     string `json:"username"`
 	LanguageCode string `json:"language_code"`
+}
+
+func MarshalTeleUser(teleUser *tele.User) *User {
+	return &User{
+		ID:           teleUser.ID,
+		FirstName:    teleUser.FirstName,
+		LastName:     teleUser.LastName,
+		Username:     teleUser.Username,
+		LanguageCode: teleUser.LanguageCode,
+	}
 }
